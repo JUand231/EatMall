1255dbf4395de330f54eedf7f96f1289859d5079
Your Name <you@example.com>
2026-04-26 19:49:58 -0500

    merge: actualizar

---
+++ EatMall/EatMall/Logica/CategoriaProductoL.cs
@@ -0,0 +1,18 @@
+﻿using EatMall.Datos;
+using EatMall.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EatMall.Logica
+{
+    public class CategoriaProductoL
+    {
+        CategoriaProductoD categoriaD = new CategoriaProductoD();
+        public List<CategoriaProducto> ObtenerCategoriasPorLocal(int idLocal)
+        {
+            return categoriaD.ObtenerCategoriasPorLocal(idLocal);
+        }
+    }
+}
